47b70a30470e8e7eb7219764792baf41dbe2606e
err34 <74163176+err34@users.noreply.github.com>
2021-10-18 20:00:05 -0400

    Add files via upload
    added functionality such as a guess counter, lets the user change how many tries they are allotted, changes the range based on allotted tries, and tells you what the number is when you get it wrong now

--- guessing game.py
+++ guessing game.py
@@ -1,22 +1,38 @@
 import random
-rand = random.randint(1, 1000)
+i = 1
+max = int(input("Enter max attempts: "))
+if max <= 5:
+    nMax = 250
+elif max < 10:
+    nMax = 500
+else:
+    nMax = 1000
+rand = random.randint(1, nMax)
+print("Guess a number between 1 and " + str(nMax))
 while True: 
-    print("Guess a number between 1 and 1000")
+    if i > max:
+        print("You ran out of tries! The number was " + str(rand))
+        cont = input("Go again? y/n ")
+        if cont == "y":
+            rand = random.randint(1, nMax)
+            i = 1
+            continue
+        else:
+            break
     guess = int(input("Enter Guess: "))
     if guess == rand:
-        cont =  input("You got it right! Go again? y/n ")
+        
+        cont =  input("You got it right! It took you " + str(i) + " guesses! Go again? y/n ")
         if cont == "y":
-            rand = random.randint(1, 1000)
+            rand = random.randint(1, nMax)
+            i = 1
             continue
         else:
             break
     else:
+        i+=1
         if guess > rand: 
             print("Too high")
         if guess < rand:
             print("Too low")
-        cont = input("Go again? y/n ")
-        if cont == "y":
-            continue
-        else:
-            break
+        continue
