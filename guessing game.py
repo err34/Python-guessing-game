de3a506415dd911dd94668ecdad95396b0717016
err34 <auburneric@gmail.com>
2021-11-15 17:06:47 -0500

    graphics!
    added graphics using tkinter

--- guessing game.py
+++ guessing game.py
@@ -1,38 +1,51 @@
-import random
-i = 1
-max = int(input("Enter max attempts: "))
-if max <= 5:
-    nMax = 250
-elif max < 10:
-    nMax = 500
-else:
-    nMax = 1000
-rand = random.randint(1, nMax)
-print("Guess a number between 1 and " + str(nMax))
-while True: 
-    if i > max:
-        print("You ran out of tries! The number was " + str(rand))
-        cont = input("Go again? y/n ")
-        if cont == "y":
-            rand = random.randint(1, nMax)
-            i = 1
-            continue
-        else:
-            break
-    guess = int(input("Enter Guess: "))
-    if guess == rand:
-        
-        cont =  input("You got it right! It took you " + str(i) + " guesses! Go again? y/n ")
-        if cont == "y":
-            rand = random.randint(1, nMax)
-            i = 1
-            continue
-        else:
-            break
+import tkinter as tk
+import random as rand
+
+ans = rand.randint(1,100)
+score = 0
+tries = 0
+root = tk.Tk()
+root.geometry("500x200")
+root.configure(bg = "#454545")
+text = tk.Label(text="enter a number between 1 and 100",foreground = "#ff00ff",bg = "#454545")
+text.pack()
+guess = tk.Entry(fg="black",bg="#676767",width =50)
+guess.pack()
+def entry(*args):
+    global guess
+    global ans
+    global text2
+    global text3
+    global text4
+    global tries
+    global score
+    str1 = guess.get()
+    int1 = int(str1)
+    text2.destroy()
+    if int1 == ans:
+        text2 = tk.Label(text="correct",foreground = "#ff00ff", bg = "#454545")
+        text2.pack()
+        score+=1
+        ans = rand.randint(1,100)
+    elif int1 > ans:
+        text2 = tk.Label(text="too high",foreground = "#ff00ff", bg = "#454545")
+        text2.pack()
     else:
-        i+=1
-        if guess > rand: 
-            print("Too high")
-        if guess < rand:
-            print("Too low")
-        continue
+        text2 = tk.Label(text="too low",foreground = "#ff00ff", bg = "#454545")
+        text2.pack()
+    text3.destroy()
+    text3 = tk.Label(text = "score is " + str(score), bg = "#454545",foreground = "#ff00ff")
+    text3.pack()
+    tries+=1
+    text4.destroy()
+    text4 = tk.Label(text = "You have made " + str(tries) + " attempts!" , bg = "#454545",foreground = "#ff00ff")
+    text4.pack()
+guess.bind('<Return>',entry)
+enter = tk.Button(text = "enter guess", command = entry, background="#676767")
+text2 = tk.Label(text="enter a guess already!")
+enter.pack()
+text3 = tk.Label(text = "score is " + str(score), bg = "#454545",foreground = "#ff00ff")
+text3.pack()
+text4 = tk.Label(text = "You have made " + str(tries) + " attempts!", bg = "#454545",foreground = "#ff00ff")
+text4.pack()
+root.mainloop()
